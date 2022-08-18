be675228bf9545af34103a696f71f1c635472f28
Jinsu Kim <jinsu.kim@tridge.com>
2022-08-18 16:28:18 +0900

    modify cmp

--- lua/others/nvim-cmp.lua
+++ lua/others/nvim-cmp.lua
@@ -96,8 +96,10 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 local languages = {
+  'eslint',
   'vimls',
   'sumneko_lua',
+  'tsserver',
 }
 
 for idx, language in ipairs(languages) do
