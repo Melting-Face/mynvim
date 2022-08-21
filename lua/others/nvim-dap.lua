c7a75c62272aab793f5fa1c2869781705b146aa2
kin <ymist17@gmail.com>
2022-08-22 00:56:31 +0900

    modify dap

--- lua/others/nvim-dap.lua
+++ lua/others/nvim-dap.lua
@@ -1,47 +1,49 @@
--- python path for dap-python
-local pythonBinPath = '/bin/python3'
-local pythonPath = ''
-if vim.fn.executable('/usr/local/Caskroom/miniforge/base' .. pythonBinPath) == 1 then
-  pythonPath = '/usr/local/Caskroom/miniforge/base' .. pythonBinPath
-elseif vim.fn.executable('/opt/homebrew/Caskroom/miniforge/base' .. pythonBinPath) == 1 then
-  pythonPath = '/opt/homebrew/Caskroom/miniforge/base' .. pythonBinPath
-elseif vim.fn.executable('/opt/homebrew' .. pythonBinPath) == 1 then
-  pythonPath = '/opt/homebrew' .. pythonBinPath
-else
-  pythonPath = '/usr/local' .. pythonBinPath
-end 
-
 -- nvim-dap
 local dap = require'dap'
 local dapui = require'dapui'
-require'dap-python'.setup(pythonPath)
+local dappython = require'dap-python'
 
--- adapter
-dap.adapters.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = {os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
-}
--- config
-dap.configurations.javascript = {
-  {
-    name = 'Launch',
-    type = 'node2',
-    request = 'launch',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-  {
-    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require'dap.utils'.pick_process,
-  },
-}
+local HOME = io.popen('echo $HOME'):read('l')
+local mason_root_dir = "/.local/share/nvim/mason"
+local python_venv = '/packages/debugpy/venv/bin/activate'
+local node_debug = '/packages/node-debug2-adapter/out/src/nodeDebug.js'
+
+if io.open(HOME .. mason_root_dir .. python_venv) ~= nil then
+  local command = 'source ' .. mason_root_dir .. python_venv
+  local python_path = io.popen(command .. ' && which python3'):read('l')
+  dappython.setup(python_path)
+end
+
+if io.open(HOME .. mason_root_dir .. node_debug) ~= nil then
+  dap.adapters.node2 = {
+    type = 'executable',
+    command = 'node',
+    args = {
+      HOME .. mason_root_dir .. node_debug,
+    },
+  }
+  -- config
+  dap.configurations.javascript = {
+    {
+      name = 'Launch',
+      type = 'node2',
+      request = 'launch',
+      program = '${file}',
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = 'inspector',
+      console = 'integratedTerminal',
+    },
+    {
+      -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+      name = 'Attach to process',
+      type = 'node2',
+      request = 'attach',
+      processId = require'dap.utils'.pick_process,
+    },
+  }
+end
+
 dap.configurations.lua = {
   {
     type = 'nlua',
@@ -60,6 +62,7 @@ dap.configurations.lua = {
     end,
   }
 }
+
 dap.adapters.nlua = function(callback, config)
   if config.port ~= nil then
     callback({
