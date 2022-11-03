95c574a8cb3a851f308d6ecb8e7856680e071e7d
Jinsu Kim <jinsukim@Jinsuui-MacBookAir.local>
2022-11-04 01:40:23 +0900

    move minimap and add nerd font

--- lua/others/minimap.lua
+++
@@ -1,5 +0,0 @@
-vim.g.minimap_width = 10
-vim.g.minimap_auto_start_win_enter = 1
-vim.g.minimap_block_filetypes = { 'gitsigns', 'nvim-tree', 'tagbar', 'fzf' }
-vim.g.minimap_close_filetypes = { 'alpha', 'vim-plug' }
-vim.g.minimap_git_colors = 1
--- lua/others/options.lua
+++ lua/others/options.lua
@@ -36,3 +36,11 @@ vim.opt.linebreak = true
 
 vim.opt.list = true
 vim.opt.listchars:append "eol:↴"
+
+vim.g.minimap_width = 10
+vim.g.minimap_auto_start_win_enter = 1
+vim.g.minimap_block_filetypes = { 'gitsigns', 'nvim-tree', 'tagbar', 'fzf' }
+vim.g.minimap_close_filetypes = { 'alpha', 'vim-plug' }
+vim.g.minimap_git_colors = 1
+vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_show_database_icon = 1
