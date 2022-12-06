7ba4aed656cd013c462eee0b6592377413ecfcc8
Jinsu Kim <jinsukim@Jinsuui-MacBookAir.local>
2022-12-07 03:39:41 +0900

    rollback dap lua

--- lua/others/nvim-dap.lua
+++ lua/others/nvim-dap.lua
@@ -22,11 +22,15 @@ if HAS_DAP == true then
   }
 
   DAP.adapters.nlua = function(callback, config)
-    callback({
-      type = 'server',
-      host = config.host or "127.0.0.1",
-      port = config.port or 8086
-    })
+    if config.port ~= nil then
+      callback({
+        type = 'server',
+        host = config.host,
+        port = config.port
+      })
+    else
+      require'osv'.run_this()
+    end
   end
 
   -- config
@@ -55,9 +59,19 @@ if HAS_DAP == true then
       type = 'nlua',
       request = 'attach',
       name = "Attach to running Neovim instance",
+      host = function()
+        local value = vim.fn.input('Host [127.0.0.1]: ')
+        if value ~= "" then
+          return value
+        end
+        return '127.0.0.1'
+      end,
+      port = function()
+        local val = tonumber(vim.fn.input('Port: '))
+        return val
+      end,
     }
   }
-
 
   DAP.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
