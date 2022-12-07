84a980e92877678f1f6c137bec80c255100e5e92
Jinsu Kim <jinsu.kim@tridge.com>
2022-12-07 18:06:53 +0900

    add isort

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -181,6 +181,7 @@ return require('packer').startup(function(use)
 
           null_ls.builtins.formatting.black,
           null_ls.builtins.formatting.eslint_d,
+          null_ls.builtins.formatting.isort,
           null_ls.builtins.formatting.remark,
           null_ls.builtins.formatting.rustfmt,
           null_ls.builtins.formatting.shellharden,
