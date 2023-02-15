8e5afeb71b2789d94a3545db0b94ee43f4f7e156
Jinsu Kim <jinsukim@Jinsuui-MacBookAir.local>
2023-02-16 00:39:23 +0900

    add codelldb

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -514,6 +514,37 @@ return {
   -- DAP
   -- dap java/typescript
   {
+    "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require('dap')
+      local mason_path = os.getenv('HOME') .. '/.local/share/nvim/mason/packages'
+      dap.adapters.codelldb = {
+        type = 'server',
+        port = "${port}",
+        executable = {
+          command = mason_path .. '/codelldb/extension/adapter/codelldb',
+          args = {"--port", "${port}"},
+        }
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = false,
+        },
+      }
+
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+    end
+  },
+  {
     "microsoft/vscode-js-debug",
     lazy = true,
     build = "npm install --legacy-peer-deps && npm run compile",
