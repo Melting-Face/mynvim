5afa34421922e377331bc818e5073f606f50b791
Jinsu Kim <jinsu.kim@tridge.com>
2023-09-18 18:16:50 +0900

    add vim move and delete autopair

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,52 +1,52 @@
 -- keymap can use string | function
 -----------------------------------
-local wk  = require('which-key')
+local wk = require("which-key")
 
 -- INFO: <leader> key (normal)
 wk.register({
   h = {
-    p = { "<cmd>cexpr system('refurb --quiet .') | copen<cr>", 'refurb dir' },
+    p = { "<cmd>cexpr system('refurb --quiet .') | copen<cr>", "refurb dir" },
   },
   t = {
-    name = 'tab & toggle',
-    c = { '<cmd>tabclose<CR>', 'tab close' },
+    name = "tab & toggle",
+    c = { "<cmd>tabclose<CR>", "tab close" },
   },
 }, {
-  prefix = '<leader>'
+  prefix = "<leader>",
 })
 
 -- INFO: <space> key
 wk.register({
   b = {
-    name = 'buffer',
-    a = { '<cmd>bufdo bd<CR>', 'buffer delete all' },
-    x = { '<cmd>bdelete<CR>', 'buffer close' },
+    name = "buffer",
+    a = { "<cmd>bufdo bd<CR>", "buffer delete all" },
+    x = { "<cmd>bdelete<CR>", "buffer close" },
   },
   t = {
-    name = 'tab',
-    n = { '<cmd>tabnew<CR>', 'new tab' },
-    x = { '<cmd>tabclose<CR>', 'tab close' },
+    name = "tab",
+    n = { "<cmd>tabnew<CR>", "new tab" },
+    x = { "<cmd>tabclose<CR>", "tab close" },
   },
 }, {
-  prefix = '<space>'
+  prefix = "<space>",
 })
 
 -- INFO: '[' key
 wk.register({
-  b = { '<cmd>bp<cr>', 'buffer previous' },
-  t = { '<cmd>tabprev<CR>', 'tab previous' },
+  b = { "<cmd>bp<cr>", "buffer previous" },
+  t = { "<cmd>tabprev<CR>", "tab previous" },
 }, {
-  prefix = '['
+  prefix = "[",
 })
 
 -- INFO: ']' key
 wk.register({
-  b = { '<cmd>bn<cr>', 'buffer next' },
-  t = { '<cmd>tabnext<CR>', 'tab next' },
+  b = { "<cmd>bn<cr>", "buffer next" },
+  t = { "<cmd>tabnext<CR>", "tab next" },
 }, {
-  prefix = ']'
+  prefix = "]",
 })
 
 -- INFO: For wrap
-vim.keymap.set('n', 'j', '"gj"', { expr = true })
-vim.keymap.set('n', 'k', '"gk"', { expr = true })
+vim.keymap.set("n", "j", '"gj"', { expr = true })
+vim.keymap.set("n", "k", '"gk"', { expr = true })
