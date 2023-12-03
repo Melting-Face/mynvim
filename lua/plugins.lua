90a11478ed14d88c6c627f5cad3bc22ce2571541
Jinsu Kim <jinsukim@Jinsuui-MacBookAir.local>
2023-12-03 22:40:19 +0900

    yaml formatter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -361,6 +361,7 @@ return {
             extra_args = { "--dialect", "snowflake" },
           }),
           null_ls.builtins.formatting.eslint_d,
+          null_ls.builtins.formatting.prettierd,
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.taplo,
           null_ls.builtins.formatting.yamlfmt,
